ca174bc7a54fb2120bd46977df4d722f0e1a4d25
Brett Derham <brettderham@Bretts-MacBook-Pro.local>
2019-02-07 20:59:03 -0800

    Add working Grammar

---
+++ syntax/parser.js
@@ -0,0 +1,11 @@
+const ohm = require('ohm-js')
+const fs = require('fs')
+const grammar = ohm.grammar(fs.readFileSync('./syntax/101Script.ohm'))
+
+module.exports = (text) => {
+  const match = grammar.match(text);
+  if (!match.succeeded()) {
+    throw match.message;
+  }
+  return match.succeeded()
+}
