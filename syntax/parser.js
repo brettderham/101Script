89270b6467331c7b1cb85ab20ef7650073b32f65
Simon Wroblewski <simonwro120@Simons-MacBook-Pro.local>
2019-02-26 20:01:00 -0800

    end merge

--- syntax/parser.js
+++ syntax/parser.js
@@ -1,5 +1,29 @@
 const ohm = require('ohm-js')
 const fs = require('fs')
+
+const Program = require('../ast/program');
+const VariableDeclaration = require('../ast/variable-declaration');
+const AssignmentStatement = require('../ast/assignment-statement');
+const BreakStatement = require('../ast/break-statement');
+const ReturnStatement = require('../ast/return-statement');
+const IfStatement = require('../ast/if-statement');
+const Case = require('../ast/case');
+const WhileStatement = require('../ast/while-statement');
+const CallStatement = require('../ast/call-statement');
+const FunctionDeclaration = require('../ast/function-declaration');
+const FunctionObject = require('../ast/function-object');
+const ListExpression = require('../ast/list-expression');
+const BinaryExpression = require('../ast/binary-expression');
+const UnaryExpression = require('../ast/unary-expression');
+const IdentifierExpression = require('../ast/identifier-expression');
+const SubscriptedExpression = require('../ast/subscripted-expression');
+const Call = require('../ast/call');
+const Parameter = require('../ast/parameter');
+const Argument = require('../ast/argument');
+const BooleanLiteral = require('../ast/boolean-literal');
+const NumericLiteral = require('../ast/numeric-literal');
+const StringLiteral = require('../ast/string-literal');
+
 const grammar = ohm.grammar(fs.readFileSync('./syntax/101Script.ohm'))
 
 module.exports = (text) => {
